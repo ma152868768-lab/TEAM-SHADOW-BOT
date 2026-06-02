5e527992c1bc93c3c6489d7ca5e72f78b313ad28
ATIF GUNJIAL <ma152868768@gmail.com>
2026-06-02 13:32:29 +0500

    Update ALIVE_MSG to include TEAM-SHADOW-MD

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",  // ADD YOUR SESSION ID 
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/atby2t.png",
-ALIVE_MSG: process.env.ALIVE_MSG || "HEY DEAR, I'm thrilled to announce that your name is ALIVE now—ready to dive into new adventures with you!",
+ALIVE_MSG: process.env.ALIVE_MSG || "HEY DEAR, I'm thrilled to announce that TEAM-SHADOW-MD is ALIVE now—ready to dive into new adventures with you!",
 SUDO_NB: process.env.SUDO_NB || "923022012664",
 AUTO_REACT: process.env.AUTO_REACT || "true", // Enable/disable auto reaction (true/false)
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
